4613f0efa259271546d29b675ce8199ea2d1b989
Jordan Vinckevleugel <115629190+jv11l@users.noreply.github.com>
2025-05-28 19:23:18 +0200

    Add type hints

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import os
 from pathlib import Path
 import piexif
 from PIL import Image
+from pydantic import BaseModel, Field, computed_field
 
-PATH_TO_FOLDER = ''
+PATH_TO_FOLDER: str = ''
 
 def input_target_folder():
     global PATH_TO_FOLDER
     cwd = os.getcwd()
-    target_folder = input("Enter the name of the target folder: ")
+    # target_folder = input("Enter the name of the target folder: ")
+    target_folder = 'images'
     target_folder = target_folder.strip('./ ')
     PATH_TO_FOLDER = Path(cwd)/target_folder
 
@@ -22,52 +24,49 @@ def input_target_folder():
 
 class MyImage:
     
-    DATETIME_FORMAT = "%Y:%m:%d %H:%M:%S"
+    DATETIME_FORMAT: str = "%Y:%m:%d %H:%M:%S"
     
-    def __init__(self, filename):
+    def __init__(self, filename: str):
         self._filename: str = filename
         
-    def __str__(self):
-        return f"File {self._filename} has datetime '{self.dt}'"
+    # def __str__(self):
+    #     return f"File {self._filename} has datetime '{self.dt}'"
         
-    def _path_to_file(self):
+    def _path_to_file(self) -> str:
         return str(PATH_TO_FOLDER/self._filename)
     
-    def _exif_dict(self):
-        path_to_file = self._path_to_file()
+    def _exif_dict(self) -> dict:
+        path_to_file: str = self._path_to_file()
         return  piexif.load(path_to_file)
     
     @property
-    def dt(self):
-        exif_dict = self._exif_dict()
+    def dt(self) -> bytes:
+        exif_dict: dict = self._exif_dict()
         return exif_dict['0th'].get(piexif.ImageIFD.DateTime)
-        
     
-    def _parse_image_datetime_from_filename(self):
+    def _parse_image_datetime_from_filename(self) -> str:
         # Strip filename prefix and file extension
-        filename_stripped = self._filename.lstrip('IMGPANO_-').rstrip('.jpg')
+        filename_stripped: str = self._filename.lstrip('IMGPANO_-').rstrip('.jpg')
         # Format filename for parsing based on filename formatting
         if 'WA' in filename_stripped:
-            filename_formatted = filename_stripped.split('-')[0]
+            filename_formatted: str = filename_stripped.split('-')[0]
         else:
-            filename_formatted = filename_stripped.replace('_', ' ')
+            filename_formatted: str = filename_stripped.replace('_', ' ')
         # Parse formatted filename as datetime
-        filename_parsed = parser.parse(filename_formatted)
+        filename_parsed: datetime = parser.parse(filename_formatted)
         # Convert datetime into formatted string
-        file_datetime = filename_parsed.strftime(self.DATETIME_FORMAT)
+        file_datetime: str = filename_parsed.strftime(self.DATETIME_FORMAT)
         return file_datetime
     
-    
-    def set_datetime(self):
-        file_datetime = self._parse_image_datetime_from_filename()
+    def set_datetime(self) -> None:
+        file_datetime: str = self._parse_image_datetime_from_filename()
         self._exif_dict['0th'][piexif.ImageIFD.DateTime] = file_datetime
-        exif_bytes = piexif.dump(self._exif_dict)  # Convert properties into bytes
+        exif_bytes: bytes = piexif.dump(self._exif_dict)  # Convert properties into bytes
         piexif.insert(exif_bytes, self._path_to_file)
 
-    def show_image(self):
+    def show_image(self) -> None:
         with Image.open(self._path_to_file) as img:
             img.show()
-
 
 if __name__ == '__main__':
     input_target_folder()
